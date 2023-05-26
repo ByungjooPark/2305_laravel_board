7a9fba183f1ece1f46e00fca0b4227cf77971394
ByungjooPark <exit>
2023-05-26 19:36:56 +0900

    0526 board 완료

---
+++ board/app/Http/Controllers/BoardsController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Boards;
+
+class BoardsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $result = Boards::select(['id', 'title', 'hits', 'created_at', 'updated_at'])->orderBy('hits', 'desc')->get();
+        return view('list')->with('data', $result);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('write');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $req
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $req)
+    {
+        $boards = new Boards([
+            'title' => $req->input('title')
+            ,'content' => $req->input('content')
+        ]);
+        $boards->save();
+        return redirect('/boards');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $boards = Boards::find($id);
+        $boards->hits++;
+        $boards->save();
+
+        return view('detail')->with('data', Boards::findOrFail($id));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $boards = Boards::find($id);
+        return view('edit')->with('data', $boards);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //DB::table('Boards')->where('id','=',$id)->update([
+        //    'title' => $request->title
+        //    ,'content' => $request->content
+        //]);
+
+        $result = Boards::find($id);
+        $result->title = $request->title;
+        $result->content = $request->content;
+        $result->save();
+
+        //return redirect('/boards/'.$id);
+        return redirect()->route('boards.show', ['board' => $id]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //Boards::destroy($id);
+
+        $board = Boards::find($id)->delete();
+        //$board->delete();
+        return redirect('/boards');
+    }
+}
