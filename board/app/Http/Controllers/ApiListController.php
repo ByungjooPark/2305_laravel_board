0e4a3e2c5255ad71f788d72ebcebdac25f7cc70f
ByungjooPark <exit>
2023-06-02 08:53:39 +0900

    0601 api, 테스트, 에러 핸들링

--- board/app/Http/Controllers/ApiListController.php
+++ board/app/Http/Controllers/ApiListController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Boards;
 
 class ApiListController extends Controller
@@ -25,6 +26,68 @@ class ApiListController extends Controller
         $arr['msg'] = 'success';
         $arr['data'] = $boards->only('id','title');
 
-        return $arr;
+        return response()->json([$arr], 404);
+        //return $arr;
+    }
+
+    function putlist(Request $req, $id) {
+        $arrData = [
+            'code'  => '0'
+            ,'msg'   => ''
+        ];
+
+        $data = $req->only('title', 'content');
+        $data['id'] = $id;
+
+        // 유효성체크
+        $validator = Validator::make($data, [
+            'id' => 'required|integer|exists:boards'
+            ,'title' => 'required|between:3,30'
+            ,'content' => 'required|max:2000'
+        ]);
+
+        if($validator->fails()) {
+            $arrData['code'] = 'E01';
+            $arrData['msg'] = 'Validate Error';
+            $arrData['errmsg'] = $validator->errors()->all();
+        } else {
+            // 업데이트 처리
+            $board = Boards::find($id);
+            $board->title = $req->title;
+            $board->content = $req->content;
+            $board->save();
+            $arrData['code'] = '0';
+            $arrData['msg'] = 'Success';
+        }
+
+        return $arrData;
+    }
+
+    function deletelist($id) {
+        $arrData = [
+            'code'  => '0'
+            ,'msg'   => ''
+        ];
+        $data['id'] = $id;
+        $validator = Validator::make($data, [
+            'id' => 'required|integer|exists:boards,id'
+        ]);
+
+        if($validator->fails()) {
+            $arrData['code'] = 'E01';
+            $arrData['msg'] = 'Error';
+            $arrData['errmsg'] = 'id not found';
+        } else {
+            $board = Boards::find($id);
+            if($board){
+                $board->delete();
+                $arrData['code'] = '0';
+                $arrData['msg'] = 'Success';
+            } else {
+                $arrData['code'] = 'E02';
+                $arrData['msg'] = 'Already Deleted';
+            }
+        }
+        return $arrData;
     }
 }
